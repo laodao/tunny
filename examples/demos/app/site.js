17a9f5ac7192cb9208ce980a30109cd97c7f131d
laodao <kingcq@gmail.com>
2012-05-11 16:01:11 +0800

    delete demo

--- examples/demos/app/site.js
+++
@@ -1,7 +0,0 @@
-var models = require('../model/models'),util = require('util');
-
-exports.index = function(form, fn) {
-	var domain = form.get('domain');
-	form.addData('domain', domain);
-	fn();
-}
